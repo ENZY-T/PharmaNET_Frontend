92b738668997e6f7f1a31cacb150ffc2e2dce81f
IsuruLakshan170 <lakshanisuru170@gmail.com>
2022-08-16 14:55:33 +0530

    icon color edit

--- src/app/components/customer/online-services/online-services.component.ts
+++ src/app/components/customer/online-services/online-services.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class OnlineServicesComponent implements OnInit {
 
   display: boolean = false;
-  
+  image1:string="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm373batch15-217-01.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=d8bbc66e02e81095950de55fcc9347f5";
+  image2:string="https://wwwnc.cdc.gov/travel/images/pharmacist-meds.jpg";
+
   constructor() { }
 
   ngOnInit(): void {
