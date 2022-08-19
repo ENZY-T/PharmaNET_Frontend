0260622d7d494f7606675652248ce2fcd4262d47
IsuruLakshan170 <lakshanisuru170@gmail.com>
2022-08-19 12:34:16 +0530

    company about data add using array

--- src/app/components/customer/about-pharmacy/about-pharmacy.component.ts
+++ src/app/components/customer/about-pharmacy/about-pharmacy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutComapany } from 'src/app/models/aboutCompany';
 
 @Component({
   selector: 'app-about-pharmacy',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPharmacyComponent implements OnInit {
 
-
+  aboutCompany: AboutComapany[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.aboutCompany = [
+      {
+          name: 'ABC Pharmacy',
+          about: 'ABC Pharmacy Limited a 100% subsidiary of Sunshine Healthcare is one of the 1st branded retail Pharmaceutical Chains in Sri Lanka that has entered the market with a view of creating a difference in the retail pharmaceutical trade. Headed by a team of professionals, ABC has introduced an innovative concept centered on superior customer care, latest technology in data management, a wide product assortment, affordable prices and a host of value additions.',
+          image: 'https://www.healthguard.lk/pub/media/Home/trans-subscribe3_1_.png'
+      }
+    ]
   }
 
 }
--- src/app/components/customer/promotions/promotions.component.ts
+++ src/app/components/customer/promotions/promotions.component.ts
@@ -41,7 +41,7 @@ export class PromotionsComponent implements OnInit {
   
 
     this.medicineCards = [
-      { id:0, name: 'Synthroid ', price:1000,image:'https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/e/r/ervenin_presoil_1.jpg',ratingNumber:3 },
+      { id:0, name: 'Synthroid ', price:1000, image:'https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/e/r/ervenin_presoil_1.jpg',ratingNumber:3 },
       { id:1, name: 'Crestor ', price:2000,image:'https://www.healthguard.lk/pub/media/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/7/3/735334_1.jpg',ratingNumber:2 },
       { id:2, name: 'Ventolin ', price:3000,image:'https://www.healthguard.lk/pub/media/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/1/8/183804_1.jpg',ratingNumber:3 },
       { id:3, name: 'Nexium ', price:4000,image:'https://www.healthguard.lk/pub/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/e/r/ervenin_presoil_1.jpg',ratingNumber:2 },
