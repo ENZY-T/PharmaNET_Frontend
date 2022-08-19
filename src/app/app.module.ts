4a4f79624957b47904c7c552b37fe182614efc05
IsuruLakshan170 <lakshanisuru170@gmail.com>
2022-08-19 21:06:37 +0530

    chat bot intergate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -66,5 +66,7 @@ import {StepsModule} from 'primeng/steps';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class AppModule {}
