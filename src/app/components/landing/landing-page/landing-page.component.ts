6eb7e40510f6a1f6a5258c02cdb76d7df93d061c
IsuruLakshan170 <82941889+IsuruLakshan170@users.noreply.github.com>
2022-12-24 11:52:26 +0530

    set

--- src/app/components/landing/landing-page/landing-page.component.ts
+++ src/app/components/landing/landing-page/landing-page.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
+  latitude:string ="";
+  longitude:string ="";
   constructor() { }
 
   ngOnInit(): void {
@@ -14,6 +15,18 @@ export class LandingPageComponent implements OnInit {
 
   onClickCreatePharmacy(){
     console.log("Navigate to create pharmcay");
+  }
+
+  onDisplayLocation(location:any){
+    console.log("this.latitude");
+    this.latitude=location.latitude;
+    this.longitude=location.longitude;
+  
+    console.log(this.latitude);
+    console.log(this.longitude);
+
+    localStorage.setItem("latitude",this.latitude);
+    localStorage.setItem("longitude",this.longitude)
   }
 }
 
